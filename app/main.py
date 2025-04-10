2bab522b6d8a4aaf997c3a4647ab6937b4055251
DevopsIntern2025 <babitharani.s@gmail.com>
2025-04-10 20:07:18 +0530

    Updated main.py file

--- app/main.py
+++ app/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     """Handle root GET request."""
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello, Bhaskar I have made changes!"}
